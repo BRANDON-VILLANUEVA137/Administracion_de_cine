cdc56bde3e798db2ed79b41fc56d6c6cf3cbb93f
Brandon Villanueva <brandonvillanueva137@gmail.com>
2025-05-06 16:08:43 -0500

    Error de cors o doom

--- public/js/Login.js
+++ public/js/Login.js
@@ -11,7 +11,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
   }
 
   try {
-    const response = await fetch('https://administraciondecine-gestion-de-cine.up.railway.app/auth/login', {
+    const response = await fetch('https://administraciondecine-gestion-de-cine.up.railway.app/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
