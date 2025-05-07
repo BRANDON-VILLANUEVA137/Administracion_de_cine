65073e2875f9872e9eb0288ecfb84516a996e4cf
Brandon Villanueva <brandonvillanueva137@gmail.com>
2025-05-06 22:05:49 -0500

    Solucion a error

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controller/authController');
 
+router.get('/check-auth', (req, res) => {
+    if (req.session && req.session.user) {
+      res.status(200).json({ authenticated: true });
+    } else {
+      res.status(401).json({ authenticated: false });
+    }
+  });
+  
+
 router.post('/login', authController.login);
 
 module.exports = router;
